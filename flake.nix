1f252ddef22b99cdeb5a3ceb4cb028cb1b2dce39
Yaroslav Bolyukin <iam@lach.pw>
2023-01-10 22:10:48 +0100

    chore: release v0.3.0

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "chainql";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+        rust = ((pkgs.rustChannelOf { date = "2022-11-20"; channel = "nightly"; }).default.override {
+          extensions = [ "rust-src" ];
+          targets = ["x86_64-unknown-linux-musl" "x86_64-unknown-linux-gnu"];
+        });
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;[
+            rust
+            cargo-edit
+          ];
+        };
+      }
+    );
+}
